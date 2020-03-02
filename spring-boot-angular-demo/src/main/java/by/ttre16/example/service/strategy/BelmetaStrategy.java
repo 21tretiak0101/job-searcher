6cb6ba18a03461e3c7e3158812033115e2ef9bf3
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-02 18:16:47 +0300

    Belmeta strategy implemented

--- spring-boot-angular-demo/src/main/java/by/ttre16/example/service/strategy/BelmetaStrategy.java
+++ spring-boot-angular-demo/src/main/java/by/ttre16/example/service/strategy/BelmetaStrategy.java
@@ -3,73 +3,125 @@ package by.ttre16.example.service.strategy;
 
 import by.ttre16.example.domain.Vacancy;
 import by.ttre16.example.dto.VacancyDto;
-import by.ttre16.example.service.Strategy;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BelmetaStrategy implements Strategy {
-    /**
-     * 1. s - Technology name
-     * 2. s - City name
-     */
-    private static final String baseURL = "https://belmeta.com/vacansii?q=Junior+%s&l=%s";
 
     private VacancyDto vacancyDto;
+    private static List<Vacancy> vacancies;
+    private static final String BASE_URL = "https://belmeta.om";
+    /**
+     * 1. %s - Technology name
+     * 2. %s - City
+     * 3. %s - Page number
+     */
+    private static final String BASE_PARAMS = "/vacansii?q=Junior+%s&l=%s&page=%d";
 
-    private static Connection connection;
 
+    @Override
+    public List<Vacancy> getVacancies() throws IOException {
 
-    public Document getDocument() throws IOException {
+        vacancies = new ArrayList<>();
+
+        for (int pageNumber = 1; true; pageNumber++) {
+            Elements vacanciesFromPage = getDocument(pageNumber).getElementsByClass("job no-logo");
+
+            if(vacanciesFromPage.size() == 0) break;
+
+            putAllVacanciesFromPage(vacanciesFromPage);
+        }
+
+        return vacancies;
+    }
+
+    @Override
+    public Document getDocument(int pageNumber) throws IOException {
         String technology = vacancyDto.getTechnology();
         String city = vacancyDto.getCity();
 
-        String URL = String.format(baseURL, technology, city);
+        String URL = BASE_URL + String.format(BASE_PARAMS, technology, city, pageNumber);
         Connection connection = Jsoup.connect(URL);
         return connection.get();
     }
 
+    @Override
+    public void putAllVacanciesFromPage(Elements vacanciesFromPage){
+        for(Element vacancyDetails: vacanciesFromPage){
+
+            String title = parseTitle(vacancyDetails);
+            if(!title.contains("Junior"))
+                continue;
+
+            Vacancy vacancy = Vacancy.builder()
+                    .title(title)
+                    .salary(parseSalary(vacancyDetails))
+                    .city(parseCity(vacancyDetails))
+                    .companyName(parseCompanyName(vacancyDetails))
+                    .url(parseUrl(vacancyDetails))
+                    .build();
+
+            vacancies.add(vacancy);
+        }
+    }
 
     public void setVacancyDto(VacancyDto vacancyDto) {
         this.vacancyDto = vacancyDto;
     }
 
+
     @Override
-    public List<Vacancy> getVacancies() {
-        return null;
+    public String parseTitle(Element element) {
+        return element.getElementsByClass("title")
+                .first()
+                .getElementsByTag("a")
+                .first()
+                .text()
+                .trim();
     }
 
     @Override
-    public String parseTitle() {
-        return null;
+    public String parseSalary(Element element) {
+        Element salaryElement = element.getElementsByClass("job-data salary").first();
+        if(salaryElement == null){
+            return "Зарплата не указана.";
+        }
+
+        return salaryElement.text().trim();
     }
 
     @Override
-    public String parseSalary() {
-        return null;
+    public String parseCity(Element element) {
+        return element.getElementsByClass("job-data region")
+                .first()
+                .attr("title")
+                .trim();
     }
 
     @Override
-    public String parseCity() {
-        return null;
+    public String parseCompanyName(Element element) {
+        return element.getElementsByClass("job-data company")
+                .first()
+                .text()
+                .trim();
     }
 
     @Override
-    public String parseCompanyName() {
-        return null;
+    public String parseUrl(Element element) {
+        return BASE_URL + element.getElementsByClass("title")
+                .first()
+                .getElementsByTag("a")
+                .first()
+                .attr("href")
+                .trim();
     }
 
-    @Override
-    public String parseSiteName() {
-        return null;
-    }
-
-    @Override
-    public String parseUrl() {
-        return null;
-    }
 }
