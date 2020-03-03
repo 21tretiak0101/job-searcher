8b697c26de9c3f72c2978950a6422600e18c3d9e
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-04 00:37:08 +0300

    simple DateFormat class added

---
+++ spring-boot-angular-demo/src/main/java/by/ttre16/example/service/strategy/dateformat/MyDateFormat.java
@@ -0,0 +1,38 @@
+package by.ttre16.example.service.strategy.dateformat;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+public class MyDateFormat {
+
+    private static final String MY_TEMPLATE = "dd MMMM";
+
+    private static DateFormatSymbols myDateFormatSymbols = new DateFormatSymbols(){
+
+        @Override
+        public String[] getMonths() {
+            return new String[]{"января", "февраля", "марта", "апреля", "мая", "июня",
+                    "июля", "августа", "сентября", "октября", "ноября", "декабря"};
+        }
+
+    };
+
+    public static String getDate(String date, String dateTemplate){
+        SimpleDateFormat localDateTemplate = new SimpleDateFormat(dateTemplate, myDateFormatSymbols);
+
+        try {
+            Date localDate = localDateTemplate.parse(date);
+            return new SimpleDateFormat(MY_TEMPLATE, myDateFormatSymbols).format(localDate);
+
+        } catch (ParseException e) {
+            log.warn("Date parsing exception with message: {}", e.getMessage());
+        }
+
+        return date;
+    }
+}
