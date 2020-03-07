6d345b33d79e2e2a50249bfa4132b67b361436dc
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-07 23:32:13 +0300

    app module modified

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {VacanciesComponent} from './shared/component/vacancies/vacancies.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgbModule, NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VacanciesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    NgbPaginationModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
