49e854a6a1615eef032977aa7919f1ab57569448
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-11 00:46:59 +0300

    interface layer added

--- spring-boot-angular-demo/src/main/java/by/ttre16/example/service/VacancyService.java
+++ spring-boot-angular-demo/src/main/java/by/ttre16/example/service/VacancyService.java
@@ -1,48 +1,10 @@
 package by.ttre16.example.service;
 
-import by.ttre16.example.domain.Vacancy;
-import by.ttre16.example.dto.VacancyDto;
-import by.ttre16.example.service.strategy.AbstractStrategy;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.ArrayList;
+import by.ttre16.example.model.Vacancy;
 import java.util.List;
 
-@Service
-@Slf4j
-public class VacancyService {
+public interface VacancyService {
 
-    private StrategyProvider provider;
+    List<Vacancy> findVacancies(String website, String city, String technology);
 
-    @Autowired
-    public VacancyService(StrategyProvider provider) {
-        this.provider = provider;
-    }
-
-    public List<Vacancy> getVacancies(VacancyDto vacancyDto){
-
-        String website = vacancyDto.getSiteName();
-        AbstractStrategy strategy = provider.getStrategy(website);
-
-        if(strategy == null){
-            log.warn("{} website strategy doesn't exist", website);
-            return null;
-        }
-
-        strategy.setVacancyDto(vacancyDto);
-
-        List<Vacancy> vacancies = null;
-
-        try {
-            vacancies = strategy.getVacancies();
-        }
-        catch (IOException e){
-            log.error("URL: connection not established with message {}", e.getMessage());
-        }
-
-        return vacancies;
-    }
 }
