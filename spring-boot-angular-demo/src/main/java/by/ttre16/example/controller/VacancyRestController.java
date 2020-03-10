bf995847f5c59a3fa0ae27e8f3fc56d3892e4b21
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-11 00:45:09 +0300

    controller params updated

--- spring-boot-angular-demo/src/main/java/by/ttre16/example/controller/VacancyRestController.java
+++ spring-boot-angular-demo/src/main/java/by/ttre16/example/controller/VacancyRestController.java
@@ -1,8 +1,8 @@
 package by.ttre16.example.controller;
 
-import by.ttre16.example.domain.Vacancy;
-import by.ttre16.example.dto.VacancyDto;
+import by.ttre16.example.model.Vacancy;
 import by.ttre16.example.service.VacancyService;
+import by.ttre16.example.service.VacancyServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,37 +11,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vacancies")
+@RequestMapping("/api/v1/jdevby")
 @CrossOrigin("http://localhost:4200")
 public class VacancyRestController {
 
-    public VacancyService service;
+    private VacancyService service;
 
     @Autowired
-    public VacancyRestController(VacancyService service) {
+    public VacancyRestController(VacancyServiceImpl service) {
         this.service = service;
     }
 
-    @GetMapping("/{city}/{technology}/{website}")
+    @GetMapping("/vacancies")
     public ResponseEntity<List<Vacancy>> getVacancies(
-            @PathVariable String city,
-            @PathVariable String technology,
-            @PathVariable String website){
+            @RequestParam(defaultValue = "belmeta") String website,
+            @RequestParam(defaultValue = "Минск") String city,
+            @RequestParam(defaultValue = "") String technology){
 
+        List<Vacancy> vacancies = service.findVacancies(website, city, technology);
 
-        VacancyDto vacancy = VacancyDto.builder()
-                .city(city)
-                .website(website)
-                .technology(technology)
-                .build();
+        return vacancies == null
+                ? new ResponseEntity<>(null, HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(vacancies, HttpStatus.OK);
 
-        System.out.println(vacancy);
-
-        List<Vacancy> vacancies = service.getVacancies(vacancy);
-        if (vacancies == null){
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(vacancies, HttpStatus.OK);
     }
 }
 
