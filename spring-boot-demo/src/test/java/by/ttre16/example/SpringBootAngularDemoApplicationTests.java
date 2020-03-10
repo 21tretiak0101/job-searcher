a70263eb551666434419800a9fc661cbe8defedf
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-11 01:15:21 +0300

    directory name changed

--- spring-boot-demo/src/test/java/by/ttre16/example/SpringBootAngularDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/by/ttre16/example/SpringBootAngularDemoApplicationTests.java
@@ -1,36 +1,13 @@
 package by.ttre16.example;
 
-import by.ttre16.example.controller.VacancyRestController;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest
-@AutoConfigureMockRestServiceServer
 class SpringBootAngularDemoApplicationTests {
 
-	@Autowired
-	private VacancyRestController restController;
-
-	@Autowired
-	private MockRestServiceServer restServiceServer;
-
 	@Test
-	void contextLoads() throws Exception {
-		//Assertions.assertThat(restController).isNotNull();
-
+	void contextLoads() {
 	}
 
 }
