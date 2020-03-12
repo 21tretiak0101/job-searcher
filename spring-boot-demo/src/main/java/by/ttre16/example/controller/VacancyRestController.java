6f09cf6429476de1c5de082aff29e325997acf04
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-12 21:03:26 +0300

    log added

--- spring-boot-demo/src/main/java/by/ttre16/example/controller/VacancyRestController.java
+++ spring-boot-demo/src/main/java/by/ttre16/example/controller/VacancyRestController.java
@@ -3,6 +3,7 @@ package by.ttre16.example.controller;
 import by.ttre16.example.model.Vacancy;
 import by.ttre16.example.service.VacancyService;
 import by.ttre16.example.service.VacancyServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/jdevby")
 @CrossOrigin("http://localhost:4200")
+@Slf4j
 public class VacancyRestController {
 
     private VacancyService service;
@@ -24,9 +26,11 @@ public class VacancyRestController {
 
     @GetMapping("/vacancies")
     public ResponseEntity<List<Vacancy>> getVacancies(
-            @RequestParam(defaultValue = "belmeta") String website,
-            @RequestParam(defaultValue = "Минск") String city,
-            @RequestParam(defaultValue = "") String technology){
+            @RequestParam(defaultValue = "belmeta", required = false) String website,
+            @RequestParam(defaultValue = "Минск", required = false) String city,
+            @RequestParam(defaultValue = "", required = false) String technology){
+
+        log.info("GET request with params: website - {}, city - {}, technology - {}", website, city, technology);
 
         List<Vacancy> vacancies = service.findVacancies(website, city, technology);
 
