55d555d7e3795e972842c53637081dfd73d02e4c
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-12 20:58:24 +0300

    interfaces created

---
+++ angular-demo/src/app/shared/interfaces.ts
@@ -0,0 +1,15 @@
+export interface Vacancy {
+  title: string;
+  text: string;
+  salary: string;
+  city: string;
+  companyName: string;
+  vacancyURL: string;
+  date: string;
+}
+
+export interface VacancyDTO {
+  website: string;
+  city: string;
+  technology: string;
+}
